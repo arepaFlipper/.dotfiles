23884c7926602e0682494bd3ee3699ba02dc258a
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-07-11 19:42:55 -0500

    🐰 neovim>set c++ Debug Adapter Protocol

--- neovim/.config/leetvim/lua/plugins/leetcode.lua
+++ neovim/.config/leetvim/lua/plugins/leetcode.lua
@@ -1,20 +1,76 @@
 return {
-  "Dhanus3133/LeetBuddy.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope.nvim",
+  {
+    "Dhanus3133/LeetBuddy.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("leetbuddy").setup({
+        language = "cpp",
+      })
+    end,
+    keys = {
+      { "ñl", "<cmd>LBQuestions<cr>", desc = "List Questions" },
+      { "ñq", "<cmd>LBQuestion<cr>", desc = "View Question" },
+      { "ñr", "<cmd>LBReset<cr>", desc = "Reset Code" },
+      { "ñt", "<cmd>LBTest<cr>", desc = "Run Code" },
+      { "ñs", "<cmd>LBSubmit<cr>", desc = "Submit Code" },
+      { "ñp", "<cmd>LBChangeLanguage<cr>", desc = "switch language" },
+    },
   },
-  config = function()
-    require("leetbuddy").setup({
-      language = "cpp",
-    })
-  end,
-  keys = {
-    { "ñl", "<cmd>LBQuestions<cr>", desc = "List Questions" },
-    { "ñq", "<cmd>LBQuestion<cr>", desc = "View Question" },
-    { "ñr", "<cmd>LBReset<cr>", desc = "Reset Code" },
-    { "ñt", "<cmd>LBTest<cr>", desc = "Run Code" },
-    { "ñs", "<cmd>LBSubmit<cr>", desc = "Submit Code" },
-    { "ñp", "<cmd>LBChangeLanguage<cr>", desc = "switch language" },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "codelldb",
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, _)
+      require("core.utils").load_mappings("dap")
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+    keys = {
+      { "ñdb", "<cmd>DapToggleBreakpoint<CR>", desc = "Add breakpoint at line" },
+      { "ñdr", "<cmd>DapContinue<CR>", desc = "Start or continue the debugger" },
+      { "ñr", "<cmd>LBReset<CR>", desc = "Reset Code" },
+      { "ñt", "<cmd>LBTest<CR>", desc = "Run Code" },
+      { "ñs", "<cmd>LBSubmit<CR>", desc = "Submit Code" },
+      { "ñp", "<cmd>LBChangeLanguage<CR>", desc = "switch language" },
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
+    },
   },
 }
