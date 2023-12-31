654204dd609a370e1ee702384b1634f53dd19cbd
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-31 15:25:21 -0500

    🌂 neovim> add some keymaps

---
+++ neovim/.config/devaslife/lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Delete a word backwards
+keymap.set("n", "dw", "vb_d")
+
+-- Select All
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- JumpList
+keymap.set("n", "<C-m>", "<C-i>", opts)
