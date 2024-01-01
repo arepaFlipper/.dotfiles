2676b8e6b86200d25f90c677f2d1037a23c8c097
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-01-01 13:21:59 -0500

    🐤 add the rest-nvim keymaps into devaslife

--- neovim/.config/devaslife/lua/config/keymaps.lua
+++ neovim/.config/devaslife/lua/config/keymaps.lua
@@ -4,6 +4,7 @@
 
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
+local Util = require("lazyvim.util")
 
 -- Delete a word backwards
 keymap.set("n", "dw", "vb_d")
@@ -13,3 +14,45 @@ keymap.set("v", "<C-a>", "<Esc>gg<S-v>G")
 
 -- JumpList
 keymap.set("n", "<C-m>", "<C-i>", opts)
+
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+map("n", "<leader>pdb", "<cmd>TodoTelescope keywords=DELETEME<cr>", { desc = "Increase window height" })
+
+vim.keymap.set("n", "<leader>ft", function()
+  Util.float_term(nil, { cwd = Util.get_root(), border = "single" })
+end, { noremap = true, silent = true, desc = "Resume" })
+
+vim.keymap.set("n", "<leader>gg", function()
+  Util.float_term({ "lazygit" }, { cwd = Util.get_root(), border = "double" })
+end, { noremap = true, silent = true, desc = "Open lazygit in floating window" })
+
+vim.keymap.set("n", "<leader>tt", function()
+  Util.float_term({ "taskwarrior-tui" }, { cwd = Util.get_root(), border = "double" })
+end, { noremap = true, silent = true, desc = "Open taskwarrior in floating window" })
+
+local status_ok, rest = pcall(require, "rest-nvim")
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "http",
+  callback = function()
+    local buff = tonumber(vim.fn.expand("<abuf>"), 10)
+    vim.keymap.set("n", "ñrr", rest.run, { noremap = true, buffer = buff })
+    vim.keymap.set(
+      "n",
+      "ñro",
+      '<cmd> lua r=require("rest-nvim");r.setup({result={show_headers=false, show_http_info=false}});r.run()<CR>',
+      { noremap = true, buffer = buff }
+    )
+    --[[ vim.keymap.set("n", "ñrl", rest.last, { noremap = true, buffer = buff }) ]]
+    vim.keymap.set("n", "ñrp", rest.run(true), { noremap = true, buffer = buff })
+  end,
+})
