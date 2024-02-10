e8b8b8f1ab4b74ffc6d1427fadead143913edf05
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-02-10 18:00:57 -0500

    🕒 tmux>add extra session to tuto

--- tmux/tuto.tmux.sh
+++ tmux/tuto.tmux.sh
@@ -8,11 +8,12 @@ session1="tuto_logs"
 # folder_name="js_mastery/pricewise"
 # folder_name="EdRoh/portfolio"
 # folder_name="Elliott-Chong/quizmify"
-folder_name="freeCodeCamp/zubiarfan/courses/Rust"
+
+folder_name="obsidian_vault/academic/rust/hello_cargo"
 window11="lazygit"
 window12="lazygit"
 window13="neovim"
-path="$HOME/Documents/yt-tutos/${folder_name}/"
+path="$HOME/Documents/${folder_name}/"
 # tmux kill-session -t $session0;
 cd ${path}
 # tmux new -s $session0;
@@ -26,21 +27,30 @@ session2="tuto_repo"
 window21="lazygit"
 session3="tuto_code"
 window31="neovim"
+session4="tuto_notes"
+window41="obsidian"
 path="$HOME/Documents/yt-tutos/${folder_name}/"
+
 # tmux kill-session -t $session0;
 cd ${path}
 # tmux new -s $session0;
 
 # session 1
 tmux new -s $session1 -n $window11 -d
-tmux send-keys -t $window11 'npm run dev' C-m
+# tmux send-keys -t $window11 'npm run dev' C-m
 tmux split-pane -t $window11.1
 
 # session 2
 tmux new -s $session2 -n $window21 -d
 tmux send-keys -t $window21 'lazygit' C-m
 
+# session 3
 tmux new -s $session3 -n $window31 -d
 tmux send-keys -t $window31 'dvim .' C-m
 
-tmux attach -t $session2:1
+# session 4
+tmux new -s $session4 -n $window41 -d
+tmux send-keys -t $session4 'cd ~/Documents/obsidian_vault/' C-m
+tmux send-keys -t $session4 'dvim .' C-m
+
+tmux attach -t $session3:1
