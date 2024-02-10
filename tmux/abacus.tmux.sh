0f8b2a7aba98681592de993ca0b39576f74bfff7
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-02-10 17:29:36 -0500

    🐟 tmux>new screen integration

--- tmux/abacus.tmux.sh
+++ tmux/abacus.tmux.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # This script sets up Tmux sessions, windows, and panes for a development environment.
 
 # -t: tag
@@ -11,20 +12,19 @@ session1="${session_name}_logs"
 window11="logs"
 pane111=${window00}.1
 window12="lazygit"
-window13="task_warrior"
 session2="${session_name}_code"
 window21="neovim"
 session3="${session_name}_repo"
 window31="lazygit"
+session4="${session_name}_db"
+window41="dvim ."
 path="$HOME/Documents/ABEXUS/${session_name}"
-task_data="$path/.task"
 
 # Navigate to the working directory
 cd "${path}/"
 
 # Session 1: Docker, Node.js, and Testing Environment
 tmux new -s $session1 -n $window11 -d
-tmux set-environment -t $session1 TASKDATA $task_data
 tmux send-keys -t $window11 'docker-compose up -d && docker-compose logs -f' C-m
 tmux split-window -v -t $window11 'cd ./frontend/client/ && npm run start'
 tmux split-pane -h -t $window11.2 'cd ./frontend/pollster/ && npm run start'
@@ -33,13 +33,15 @@ tmux split-pane -h -t $window11.1 'cd ./tests/ && npm run test'
 
 # Session 2: Code Editing
 tmux new -s $session2 -n $window21 -d
-tmux set-environment -t $session2 TASKDATA $task_data
 tmux send-keys -t $session2:1 'dvim .' C-m
 
 # Session 3 repository: LazyGit
 tmux new -s $session3 -n $window31 -d
-tmux set-environment -t $session1 TASKDATA $task_data
 tmux send-keys -t $session3:1 'lazygit' C-m
 
-# Attach to Session 1 to start development environment
-tmux attach -t $session2:1
+# Session 4 repository: DadBod Database
+tmux new -s $session4 -d
+tmux send-keys -t $session4 'dvim .' C-m
+
+# Attach to Session 2 to start development environment
+tmux attach -t $session4
