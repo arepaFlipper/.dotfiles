165ba2f25a55a80859db36c09be1e8915f6d4f2c
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-02-15 10:59:18 -0500

    😯 codeium refactor

--- neovim/.config/devaslife/lua/plugins/codeium.lua
+++ neovim/.config/devaslife/lua/plugins/codeium.lua
@@ -1,38 +1,46 @@
 return {
-  "Exafunction/codeium.vim",
-  config = function()
-    -- Chang<c-g>e '<C-g>' here to any keycode you like.
-    -- vim.g.codeium_no_map_tab = true
-    --
-    --- execute at the en end of the file only
-    --- execute at the en end of the file only
+  {
+    "Exafunction/codeium.vim",
+    config = function()
+      -- Chang<c-g>e '<C-g>' here to any keycode you like.
+      -- vim.g.codeium_no_map_tab = true
+      --
+      --- execute at the en end of the file only
+      --- execute at the en end of the file only
 
-    vim.keymap.set("i", "<tab>", function()
-      local col = vim.fn.col(".")
-      local line_len = vim.fn.col("$")
-      if col ~= line_len then
-        return vim.fn["codeium#Accept"]()
-      else
-        return vim.fn["<right>"]()
-      end
-    end, { expr = true })
-    vim.keymap.set("i", "<right>", function()
-      local col = vim.fn.col(".")
-      local line_len = vim.fn.col("$")
-      if col == line_len then
-        return vim.fn["codeium#Accept"]()
-      else
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Right>", true, true, true), "n", true)
-      end
-    end, { expr = true })
-    vim.keymap.set("i", "<c-;>", function()
-      return vim.fn["codeium#CycleCompletions"](1)
-    end, { expr = true })
-    vim.keymap.set("i", "<c-,>", function()
-      return vim.fn["codeium#CycleCompletions"](-1)
-    end, { expr = true })
-    vim.keymap.set("i", "<c-x>", function()
-      return vim.fn["codeium#Clear"]()
-    end, { expr = true })
-  end,
+      vim.keymap.set("i", "<tab>", function()
+        local col = vim.fn.col(".")
+        local line_len = vim.fn.col("$")
+        if col ~= line_len then
+          return vim.fn["codeium#Accept"]()
+        else
+          return vim.fn["<right>"]()
+        end
+      end, { expr = true })
+      vim.keymap.set("i", "<right>", function()
+        local col = vim.fn.col(".")
+        local line_len = vim.fn.col("$")
+        if col == line_len then
+          return vim.fn["codeium#Accept"]()
+        else
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Right>", true, true, true), "n", true)
+        end
+      end, { expr = true })
+      vim.keymap.set("i", "<c-;>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true })
+      vim.keymap.set("i", "<c-,>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true })
+      vim.keymap.set("i", "<c-x>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true })
+    end,
+  },
+  {
+    "antonk52/markdowny.nvim",
+    config = function()
+      require("markdowny").setup({ filetypes = { "markdown", "txt" } })
+    end,
+  },
 }
