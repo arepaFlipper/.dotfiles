e46ddc0a6644c1a4cec22c6c6749678c4532ec85
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-03-08 10:58:29 -0500

    🍞 taskwarrior>remove taskwarrior

--- taskwarrior/.task/meditations.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-task add "Meditate 15 at 8" priority:L project:routine tags:mindset,care due:8:00
-task add "Meditate 15 at 12" priority:L project:routine tags:mindset,care due:12:00
-task add "Meditate 15 at 4" priority:L project:routine tags:mindset,care due:16:00
