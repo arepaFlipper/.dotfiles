b5b2036a15dbd80bca509a2a08607fd2e1a46817
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-05-27 05:33:57 -0500

    🎉 tweak the microservices project launch script

--- tmux/microservices.tmux.sh
+++ tmux/microservices.tmux.sh
@@ -13,7 +13,7 @@ folder_name="2-Areas/academic/microservices/"
 window11="lazygit"
 window12="lazygit"
 window13="neovim"
-path="/tmp/micro"
+path="/home/tovar/Documents/yt-tutos/freeCodeCamp/micro"
 # tmux kill-session -t $session0;
 cd ${path}
 # tmux new -s $session0;
@@ -34,20 +34,22 @@ window41="obsidian"
 cd ${path}
 # tmux new -s $session0;
 
-# session 1
+# session 1 (logs)
 tmux new -s $session1 -n $window11 -d
-# tmux send-keys -t $window11 'npm run dev' C-m
+tmux send-keys -t $window11 'minikube tunnel --bind-address rabbitmq-manager.com' C-m
 tmux split-pane -t $window11.1
+tmux split-pane -t -h $window11.2
 
-# session 2
+# session 2 (git)
 tmux new -s $session2 -n $window21 -d
 tmux send-keys -t $window21 'lazygit' C-m
+tmux new-window -t $session2 -n "k9s" 'k9s'
 
-# session 3
+# session 3 (code)
 tmux new -s $session3 -n $window31 -d
 tmux send-keys -t $window31 'dvim .' C-m
 
-# session 4
+# session 4 (notes)
 tmux new -s $session4 -n $window41 -d
 tmux send-keys -t $session4 'cd /iPad_sync/obsidian_vault/ && dvim .' C-m
 
