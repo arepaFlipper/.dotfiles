537841042a40a5f4a0aad7a31666f81269084b83
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-12-30 08:41:42 -0500

    🦖 neovim>fix emojis in lazyvim distro to allow commit prefixing

---
+++ foo.py
@@ -0,0 +1,11 @@
+print("""🥸   \x1b[1;33;40mfoo.py:1 DELETEME::""") ## DELETEME:
+print(DELETEME:) ## DELETEME:
+      print("""🍖   \x1b[1;33;40mfoo.py:3   print(\x1b[0m) ## DELETEME::""") ## DELETEME:
+      print(print('\x1b[0m') ## DELETEME:) ## DELETEME:
+      print('\x1b[0m') ## DELETEME:
+print("""🐢   \x1b[1;33;40mfoo.py:6 print(\x1b[0m) ## DELETEME::""") ## DELETEME:
+print(print('\x1b[0m') ## DELETEME:) ## DELETEME:
+print('\x1b[0m') ## DELETEME:
+      print("""😉   \x1b[1;33;40mfoo.py:9   dvim:""") ## DELETEME:
+      print(dvim) ## DELETEME:
+      print('\x1b[0m') ## DELETEME:
