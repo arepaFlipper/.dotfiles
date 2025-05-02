849cc3a213b4927835a023f73662df3473cdda58
Cristian F. Tovar <cristian00tovar@gmail.com>
2025-05-02 11:41:53 -0500

    😵 neovim>fix debugger macros in bvim distro

--- neovim/.config/bvim/init.lua
+++ neovim/.config/bvim/init.lua
@@ -8,25 +8,25 @@ vim.g.mapleader = " "
 local function get_random_emoji()
   -- Load emojis
   local emojis = dofile(vim.fn.expand("~/.dotfiles/neovim/.config/snippets_lua/emojis.lua")).Emojis
-  local emoji  = emojis[math.random(#emojis)]
+  local emoji = emojis[math.random(#emojis)]
   return emoji
 end
 
 local function compute_color_code(ln)
   local code = math.floor(250 - 50000 * ((ln + 40.5) ^ 0.9 + 6) ^ -1.5) * 0x10000
   code = code * 255
-  return "#"..string.sub(string.format("%x", code), 1, 6)
+  return "#" .. string.sub(string.format("%x", code), 1, 6)
 end
 
 -- Main function to insert fancy console.log
 local function insert_js_log()
-  local file_name  = vim.fn.expand("%:t")
-  local line_number  = vim.fn.line(".")
-  local selected_text  = vim.fn.getreg("+"):gsub("[%r\n]+", "")
+  local file_name = vim.fn.expand("%:t")
+  local line_number = vim.fn.line(".")
+  local selected_text = vim.fn.getreg("+"):gsub("[%r\n]+", "")
   -- Load emojis
   local emojis = dofile(vim.fn.expand("~/.dotfiles/neovim/.config/snippets_lua/emojis.lua")).Emojis
-  local emoji  = emojis[math.random(#emojis)]
-  local color  = compute_color_code(line_number)
+  local emoji = emojis[math.random(#emojis)]
+  local color = compute_color_code(line_number)
 
   -- Get selected text (yanked to + register)
   local selected_text = vim.fn.getreg("+")
@@ -44,12 +44,15 @@ local function insert_js_log()
 
   local log1 = string.format(
     "console.log(`%s%%c%s:%d - %s`, 'font-weight:bold; background:%s;color:#fff;'); //DELETEME:",
-    emoji, file_name, line_number, selected_text, color
+    emoji,
+    file_name,
+    line_number,
+    selected_text,
+    color
   )
   local log2 = string.format("console.log(%s); // DELETEME:", selected_text)
 
   vim.api.nvim_put({ log1, log2 }, "l", true, true)
-
 end
 
 -- Make the function globally accessible
@@ -72,12 +75,76 @@ vim.api.nvim_create_autocmd("FileType", {
     local enter = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
     local macro = table.concat({
       esc,
-      "gv\"+y",
+      'gv"+y',
       esc,
       ":lua insert_js_log()",
-      enter
+      enter,
     })
     vim.fn.setreg("p", macro)
   end,
 })
 
+local colors = {
+  "\\x1b[1;30;41m",
+  "\\x1b[1;30;42m",
+  "\\x1b[1;30;43m",
+  "\\x1b[1;30;44m",
+  "\\x1b[1;30;45m",
+  "\\x1b[1;30;46m",
+  "\\x1b[1;30;47m",
+  "\\x1b[1;37;41m",
+  "\\x1b[1;37;42m",
+  "\\x1b[1;37;44m",
+  "\\x1b[1;37;45m",
+  "\\x1b[1;37;46m",
+}
+
+local function get_random_emoji()
+  local emojis = dofile(vim.fn.expand("~/.dotfiles/neovim/.config/snippets_lua/emojis.lua")).Emojis
+  return emojis[math.random(#emojis)]
+end
+
+local function get_random_color()
+  return colors[math.random(#colors)]
+end
+
+-- 3. Main inserter for Python debug print
+local function insert_py_log()
+  local emoji = get_random_emoji()
+  local color = get_random_color()
+  local file = vim.fn.expand("%:t")
+  local line = vim.fn.line(".")
+  local txt = vim.fn.getreg("+"):gsub("[%r\n]+", "")
+
+  if txt == "" then
+    vim.notify("No text selected", vim.log.levels.ERROR)
+    return
+  end
+
+  local log1 = string.format('  print("""%s   %s%s:%d  %s:""") ## DELETEME:', emoji, color, file, line, txt)
+  local log2 = string.format("  print(%s) ## DELETEME:", txt)
+  local log3 = "  print('\\x1b[0m') ## DELETEME:"
+
+  vim.api.nvim_put({ log1, log2, log3 }, "l", true, true)
+end
+
+_G.insert_py_log = insert_py_log
+
+-- 4. Autocommand & macro for Python files
+vim.api.nvim_create_augroup("PyLogMacro", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = "PyLogMacro",
+  pattern = "python",
+  callback = function()
+    local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+    local cr = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
+    local macro = table.concat({
+      esc,
+      'gv"+y', -- reselect + yank to +
+      esc,
+      ":lua insert_py_log()", -- call the Lua inserter
+      cr,
+    })
+    vim.fn.setreg("p", macro)
+  end,
+})
--- neovim/.config/leetvim/init.lua
+++ neovim/.config/leetvim/init.lua
@@ -1,2 +1,69 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- 1. Load LazyVim and set leader
 require("config.lazy")
+vim.g.mapleader = " "
+
+-- 2. Helpers: emojis & colors
+local emojis = dofile(vim.fn.expand("~/.dotfiles/neovim/.config/snippets_lua/emojis.lua")).Emojis
+local colors = {
+  "\\x1b[1;30;41m",
+  "\\x1b[1;30;42m",
+  "\\x1b[1;30;43m",
+  "\\x1b[1;30;44m",
+  "\\x1b[1;30;45m",
+  "\\x1b[1;30;46m",
+  "\\x1b[1;30;47m",
+  "\\x1b[1;37;41m",
+  "\\x1b[1;37;42m",
+  "\\x1b[1;37;44m",
+  "\\x1b[1;37;45m",
+  "\\x1b[1;37;46m",
+}
+
+local function get_random_emoji()
+  return emojis[math.random(#emojis)]
+end
+
+local function get_random_color()
+  return colors[math.random(#colors)]
+end
+
+-- 3. Main inserter for Python debug print
+local function insert_py_log()
+  local emoji = get_random_emoji()
+  local color = get_random_color()
+  local file = vim.fn.expand("%:t")
+  local line = vim.fn.line(".")
+  local txt = vim.fn.getreg("+"):gsub("[%r\n]+", "")
+
+  if txt == "" then
+    vim.notify("No text selected", vim.log.levels.ERROR)
+    return
+  end
+
+  local log1 = string.format('  print("""%s   %s%s:%d  %s:""") ## DELETEME:', emoji, color, file, line, txt)
+  local log2 = string.format("  print(%s) ## DELETEME:", txt)
+  local log3 = "  print('\\x1b[0m') ## DELETEME:"
+
+  vim.api.nvim_put({ log1, log2, log3 }, "l", true, true)
+end
+
+_G.insert_py_log = insert_py_log
+
+-- 4. Autocommand & macro for Python files
+vim.api.nvim_create_augroup("PyLogMacro", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = "PyLogMacro",
+  pattern = "python",
+  callback = function()
+    local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+    local cr = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
+    local macro = table.concat({
+      esc,
+      'gv"+y', -- reselect + yank to +
+      esc,
+      ":lua insert_py_log()", -- call the Lua inserter
+      cr,
+    })
+    vim.fn.setreg("p", macro)
+  end,
+})
