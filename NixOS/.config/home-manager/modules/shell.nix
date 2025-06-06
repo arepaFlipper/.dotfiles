2994ad7244fa79841cbfd0df537903d46754c1e0
Cristian F. Tovar <cristian00tovar@gmail.com>
2025-06-06 17:30:59 -0500

    💙 zsh> update the p10k for arch

--- NixOS/.config/home-manager/modules/shell.nix
+++ NixOS/.config/home-manager/modules/shell.nix
@@ -1,54 +1,44 @@
 { config, pkgs, ... }:
 let
-  unstable = import <nixos-unstable> { config = {allowUnfree = true;};};
-  home = {
-
-    packages = with pkgs; [
-        pkgs.zsh-autosuggestions
-        pkgs.zsh-syntax-highlighting
-        pkgs.zoxide
-        yt-dlp
-        fzf
-        (pkgs.nerdfonts.override { fonts = [ "Meslo" "FiraCode" "DroidSansMono" "JetBrainsMono" ]; })
-    ];
-  };
-in 
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in
 {
+  home.packages = with pkgs; [
+    pkgs.zsh-autosuggestions
+    python313Packages.pip
+    pkgs.zsh-syntax-highlighting
+    zoxide
+    alacritty
+    yt-dlp
+    fzf
+    (pkgs.nerdfonts.override { fonts = [ "Meslo" "FiraCode" "DroidSansMono" "JetBrainsMono" ]; })
+  ];
+
   programs = {
     zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      oh-my-zsh = {
         enable = true;
-        enableCompletion = true;
+        plugins = [ "git" "sudo" "tmux" "pip" ];
+      };
 
-        autosuggestion.enable = true;
-        syntaxHighlighting = {
-            enable = true;
-        };
-
-        oh-my-zsh = {
-          enable = true;
-          plugins = [ 
-            "git" 
-            "sudo" 
-            "tmux" 
-            "pip" 
-          ];
-        };
-
-          initExtra = ''
-            alias hmsi="home-manager switch --impure"
-            alias nxrb="sudo nixos-rebuild switch --flake ~/.dotfiles/NixOS/.config/home-manager"
-            source $HOME/.dotfiles/zsh/.zshrc
-            source $HOME/.dotfiles/NixOS/.p10k.zsh
-          '';
+      initExtra = ''
+        alias hmsi="home-manager switch --impure"
+        alias nxrb="sudo nixos-rebuild switch --flake ~/.dotfiles/NixOS/.config/home-manager"
+        source $HOME/.dotfiles/zsh/.zshrc
+      '';
     };
+
     bash = {
       enable = true;
-        
       initExtra = ''
-        # include .profile if it exists
         [[ -f ~/.profile ]] && . ~/.profile
       '';
     };
   };
-
 }
+
