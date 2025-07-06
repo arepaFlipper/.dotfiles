1a18b04fedd6d4b99a3e04903a9c2b1278050c2d
Cristian F. Tovar <cristian00tovar@gmail.com>
2025-07-05 19:19:14 -0500

    💮 tmux> fix the tmux config files sourcing

--- NixArch/.config/home-manager/modules/tmux.nix
+++ NixArch/.config/home-manager/modules/tmux.nix
@@ -4,70 +4,28 @@ with lib;
 
 let
 
-  cfg = config.programs.tmux;
-
-  pluginName = p: if types.package.check p then p.pname else p.plugin.pname;
-
-  pluginModule = types.submodule {
-    options = {
-      plugin = mkOption {
-        type = types.package;
-        description = "Path of the configuration file to include.";
-      };
-
-      extraConfig = mkOption {
-        type = types.lines;
-        description = "Additional configuration for the associated plugin.";
-        default = "";
-      };
-    };
-  };
-
   defaultKeyMode = "vim";
   defaultResize = 5;
   defaultShortcut = "b";
   defaultTerminal = "screen";
-  defaultShell = null;
+  defaultShell = "zsh";
 
   boolToStr = value: if value then "on" else "off";
 
   tmuxConf = ''
-    source ~/.zshrc
+    source $HOME/.dotfiles/tmux/.tmux.conf
   '';
 
-  configPlugins = {
-    assertions = [
-      (let
-        hasBadPluginName = p: !(hasPrefix "tmuxplugin" (pluginName p));
-        badPlugins = filter hasBadPluginName cfg.plugins;
-      in {
-        assertion = badPlugins == [ ];
-        message = ''Invalid tmux plugin (not prefixed with "tmuxplugins"): ''
-          + concatMapStringsSep ", " pluginName badPlugins;
-      })
-    ];
-
-    xdg.configFile."tmux/tmux.conf".text = ''
-      # ============================================= #
-      # Load plugins with Home Manager                #
-      # --------------------------------------------- #
-
-      ${(concatMapStringsSep "\n\n" (p: ''
-        # ${pluginName p}
-        # ---------------------
-        ${p.extraConfig or ""}
-        run-shell ${if types.package.check p then p.rtp else p.plugin.rtp}
-      '') cfg.plugins)}
-      # ============================================= #
-    '';
-  };
 
 in {
+
+  home.file.".tmux.conf".source = ../../../../tmux/.tmux.conf;
   programs.tmux = {
     enable = true;
     mouse = true;
     keyMode = "vi";
     extraConfig = ''
+      source $HOME/.dotfiles/tmux/.tmux.conf
     '';
 
   };
