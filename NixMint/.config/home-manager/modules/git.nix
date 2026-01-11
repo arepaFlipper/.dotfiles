eb60114851ae6dd145a4e7bfe8f596b31f054c3c
Cristian F. Tovar <cristian00tovar@gmail.com>
2026-01-11 18:37:28 -0500

    🦉 nix>use home-manager to symlink git config files

--- NixMint/.config/home-manager/modules/git.nix
+++ NixMint/.config/home-manager/modules/git.nix
@@ -4,6 +4,22 @@
     delta
     lazygit
   ];
+
+  home.file = {
+		"~/.gitattributes" = {
+			source = "${config.home.homeDirectory}/.dotfiles/git/.gitattributes";
+			target = "${config.home.homeDirectory}/.gitattributes";
+		};
+		"~/.gitconfig" = {
+			source = "${config.home.homeDirectory}/.dotfiles/git/.gitconfig";
+			target = "${config.home.homeDirectory}/.gitconfig";
+		};
+		"~/.githooks" = {
+			source = "${config.home.homeDirectory}/.dotfiles/git/.githooks";
+			target = "${config.home.homeDirectory}/.githooks";
+		};
+	};
+
   programs.lazygit = {
   	enable = true;
   };
