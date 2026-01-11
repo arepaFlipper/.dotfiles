be789ca06d88a020cf1344dfe182192f9cbf4e9c
Cristian F. Tovar <cristian00tovar@gmail.com>
2026-01-11 18:39:23 -0500

    📏 Nix> set a customizable shell

--- NixMint/.config/home-manager/modules/shell.nix
+++ NixMint/.config/home-manager/modules/shell.nix
@@ -3,42 +3,69 @@
 let
   unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
 in {
-  home.packages = with pkgs; [
-    zsh-autosuggestions
-    zsh-powerlevel10k
-    python313Packages.pip
-    zsh-syntax-highlighting
-    zoxide
-    yt-dlp
-    fzf
-    oh-my-zsh
-  ];
 
-  home.file.".p10k.zsh".source = ../../../../zsh/.p10k.arch.zsh;
+	home = {
+		packages = with pkgs; [
+			python315
+			zoxide
+			yt-dlp
+			fzf
+			unzip
+			starship
+			fd
+		];
+
+		file = {
+			".p10k.zsh".source = ../../../../zsh/.p10k.arch.zsh;
+			".config/starship.toml".source = ../../../../starship/.config/mint.toml;
+			".config/bvim".source = ../../../../neovim/.config/bvim;
+		};
+
+	};
 
   programs = {
     zsh = {
       enable = true;
       enableCompletion = true;
-      autosuggestion.enable = true;
       syntaxHighlighting.enable = true;
+      autosuggestion.enable = true;
+
+      initContent = ''
+        source ~/.p10k.zsh
+        alias hmsi="home-manager switch --flake ~/.dotfiles/NixMint/.config/home-manager --impure"
+        alias nxrb="sudo nixos-rebuild switch --flake ~/.dotfiles/NixMint/.config/home-manager"
+      '';
+
+			shellAliases = {
+				la = "ls -lha";
+			};
 
       oh-my-zsh = {
         enable = true;
         plugins = [ "git" "sudo" "tmux" "pip" ];
+        theme = "robbyrussell";
       };
 
-      initExtra = ''
-        source ~/.p10k.zsh
-        alias hmsi="home-manager switch --impure"
-        alias nxrb="sudo nixos-rebuild switch --flake ~/.dotfiles/NixArch/.config/home-manager"
-        source $HOME/.dotfiles/zsh/.zshrc
-        if [ -n "$BASH_VERSION" ] && [ -f "$HOME/.nix-profile/bin/zsh" ]; then
-            export SHELL="$HOME/.nix-profile/bin/zsh"
-            exec "$HOME/.nix-profile/bin/zsh"
-        fi
-      '';
     };
+
+		starship = {
+			enable = true;
+			enableZshIntegration = true;
+		};
+
+		bash = {
+			enable = false;
+			bashrcExtra = ''
+				zsh
+			'';
+		};
+
+		zoxide = {
+			enable = true;
+			enableZshIntegration = true;
+
+		};
+
   };
 }
 
