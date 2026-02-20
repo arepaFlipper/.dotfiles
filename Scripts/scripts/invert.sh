c23226d340ad2021badcb2e1bbbcad946090f929
Cristian F. Tovar <cristian00tovar@gmail.com>
2026-02-20 05:58:44 -0500

    😪 Scripts: fix the picom invert issue because ID deprecation 🙄

--- Scripts/scripts/invert.sh
+++ Scripts/scripts/invert.sh
@@ -1,12 +1,17 @@
-#! /bin/sh
+#!/bin/sh
 
-if [ "$(pidof pid)" ]; then
-  pkill pid
+# If picom is already running, kill it to "turn off" the inversion
+if pgrep -x "picom" >/dev/null; then
+  pkill -x picom
 else
+  # Get the ID of the currently focused window
+  # We use xdotool which should be available in the user's environment
+  ID=$(xdotool getwindowfocus getwindowclassname)
 
-  ID=$(xdotool getactivewindow)
-  CLASS=$(xprop -id "$ID" | grep "WM_CLASS" | awk '{print $4}')
-  COND="class_g=${CLASS}"
-  picom --invert-color-include "class_g='$COND'" &
+  if [ -n "$ID" ]; then
+    # Start picom and invert only the window with the given ID.
+    # We use the xrender backend for better compatibility with this type of inversion,
+    # similar to the synergy_invert.sh script.
+    picom --backend xrender --invert-color-include "class_g = '$ID'" &
+  fi
 fi
-exit
