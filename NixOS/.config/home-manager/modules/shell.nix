da37636ff9b9c333754fb3e3aa45fed149a166ac
Cristian F. Tovar <cristian00tovar@gmail.com>
2026-07-06 04:53:34 -0500

    🤵 (feat): add shortcuts to quickly reset x11

--- NixOS/.config/home-manager/modules/shell.nix
+++ NixOS/.config/home-manager/modules/shell.nix
@@ -26,6 +26,10 @@
       initContent = ''
         alias hmsi="home-manager switch --impure"
         alias nxrb="sudo nixos-rebuild switch --flake ~/.dotfiles/NixOS/.config/home-manager"
+        alias xres="sudo systemctl restart display-manager"
+        alias kx11="sudo pkill -9 -f 'bin/X vt1'"
+        alias ksyn="sudo pkill synergy"
+        alias xwork="xrandr --output DP-1 --rate 59.98 --brightness 0.3 --mode 5120x1440"
         source $HOME/.dotfiles/zsh/.zshrc
       '';
     };
